2e88772d822ae767fb15604f441c81d0edc8e475
ugochukwudev <paulambrose5002@gmail.com>
2024-11-20 12:59:07 +0100

    first commit

---
+++ bot/index.ts
@@ -0,0 +1,47 @@
+const { Telegraf } = require('telegraf');
+const { BOT_TOKEN, WEBAPP_URL } = require('./config');
+
+if (!BOT_TOKEN) {
+  throw new Error('BOT_TOKEN must be provided!');
+}
+
+const bot = new Telegraf(BOT_TOKEN);
+
+// Basic commands
+bot.command('start', (ctx: any) => {
+  ctx.reply('Welcome to TaskVaultBot! 🚀\nUse /help to see available commands.');
+});
+
+bot.command('help', (ctx: any) => {
+  ctx.reply(
+    'Available commands:\n' +
+    '/start - Start the bot\n' +
+    '/help - Show this help message\n' +
+    '/webapp - Open the Mini App'
+  );
+});
+
+bot.command('webapp', (ctx: any) => {
+  const chatId = ctx.chat.id;
+  // Encode le chatId en base64
+  const encodedGroupId = Buffer.from(chatId.toString()).toString('base64');
+  
+  console.log('Chat ID:', chatId);
+  console.log('Encoded Group ID:', encodedGroupId);
+  
+  ctx.reply('Open Web App', {
+    reply_markup: {
+      inline_keyboard: [[
+        { text: "Open App", url: `${WEBAPP_URL}?startapp=${encodedGroupId}` }
+      ]]
+    }
+  });
+});
+
+bot.launch().then(() => {
+  console.log('Bot is running...');
+});
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
